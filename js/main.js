51a10c5e8ddeb3197ff643ba5b363f0fdb00d973
Oscar Quinteros <oquinter@gmu.edu>
2016-04-05 18:10:34 -0400

    mouse stuff

--- js/main.js
+++ js/main.js
@@ -16,6 +16,23 @@ var health;
 var spaces;
 var walls;
 var spookies;
+var arrows;
+
+var playerLayer;
+var arrowLayer;
+var floorLayer;
+
+var arrowOrigin;
+var arrowEnd;
+var arrowTurn;
+var arrowForward;
+var lastArrow;
+
+var totalMoves;
+var moves;
+
+var destX;
+var destY;
 
 var space;
 var wall;
@@ -33,30 +50,38 @@ var scoreText;
 var score;
 var gameOver;
 function create() {
-    
     music = game.add.audio('music');
     music.loop = true;
     music.play();
     
+    totalMoves = 2;
+    moves = 0;
     gameOver = false; //the game can't end before it starts
     yourTurn = true; //start with the player's turn
-    spaces = game.add.group();
+    floorLayer = game.add.group();
+    arrowLayer = game.add.group();
+    playerLayer = game.add.group();
     for (var x = 0; x < 12; x++){
         for (var y = 0; y < 12; y++){
-            space = spaces.create(x * 64, y * 64, 'paddleAtlas', 'space'); //spaces in every other space
+            space = floorLayer.create(x * 64, y * 64, 'paddleAtlas', 'space');
+            space.inputEnabled = true;
+            space.input.useHandCursor = true;
+            space.events.onInputOver.add(drawArrow, this);
         }
     }
-    laddle = game.add.sprite(66, 90, 'paddleAtlas', 'laddle_01');
-    laddle.animations.add('express', Phaser.Animation.generateFrameNames('laddle_', 1, 15, '', 1), 15, true); //create the animation of player
-    laddle.animations.play('express');
-    
+    laddle = playerLayer.create(67, 90, 'paddleAtlas', 'laddle_01');
+    laddle.animations.add('express', Phaser.Animation.generateFrameNames('laddle_', 1, 15, '', 2), 4, true); //create the animation of player
+    laddle.animations.play('express'); 
+    destX = laddle.world.x;
+    destY = laddle.world.y;
+
     spookies = game.add.group();    
 
     for (var x = 1; x < 11; x++){
         for (var y = 1; y < 11; y++){
             place = Math.floor(Math.random() * 25);
             if(place < 2 && x != 1 && y != 1){ //2/25 chance of placing down a ghost
-                spooky = spookies.create(x * 64, y * 64, 'paddleAtlas', 'ghost_1'); //place it right on a tile
+                spooky = spookies.create(x * 64+6, y * 64, 'paddleAtlas', 'ghost_1'); //place it right on a tile
                 spooky.animations.add('rattleLeft', Phaser.Animation.generateFrameNames('ghost_left_', 1, 2, '', 1), 2, true);
                 spooky.animations.play('rattleLeft');
             }
@@ -74,6 +99,7 @@ function create() {
 
 function update () {
     //call on the different move functions when we hit a button. onDown makes it so it only works once per press
+    //game.input.onDown.add(moveDown, this);
     down.onDown.add(moveDown);
     up.onDown.add(moveUp);
     left.onDown.add(moveLeft);
@@ -82,8 +108,20 @@ function update () {
     hit(); //checks for collsions
 }
 
+function moveTo(){
+    
+}
+
 var laddleTween;
 var block;
+function drawArrow(){
+    if (Math.abs(game.input.mousePointer.y - laddle.world.y) <= 224 && Math.abs(game.input.mousePointer.y - laddle.world.y) >= 32){
+        arrowOrigin = arrowLayer.create(laddle.world.x+29, laddle.world.y+6, 'paddleAtlas', 'arrow_origin');
+        arrowOrigin.anchor.setTo(0, 0.5);
+        arrowOrigin.angle = 90;
+    }
+}
+
 function moveDown(){
     block = false; //we always start block with false. Decides if theree's a wall in our way
     /*for (var w = 0; w < walls.length; w ++){ //go through each wall
@@ -92,14 +130,43 @@ function moveDown(){
             block = true; //if there's a wall in the way, we set block to true
         }
     }*/
-       
-    if(yourTurn && !gameOver && !block){ //only works if it is the player's turn, the game isn't over, and there isn't a block in the way
+    if (yourTurn && !gameOver){
+        move++;
+        if (moves <= totalMoves){
+            destX += 64;
+            if (moves == 1){
+                arrowOrigin = arrowLayer.create(laddle.world.x+29, laddle.world.y+6, 'paddleAtlas', 'arrow_origin');
+                arrowOrigin.anchor.setTo(0, 0.5);
+                arrowOrigin.angle = 90;
+
+                arrowEnd = arrowLayer.create(0, 32, 'paddleAtlas', 'arrow_end');
+                arrowEnd.anchor.setTo(0, 0.5);
+                arrowEnd.angle = 90;
+                arrowOrigin.setChild(arrowEnd);
+            }
+            /*else{
+                //arrowEnd.destroy();
+                arrowOrigin = arrowLayer.create(laddle.world.x+29, laddle.world.y+6, 'paddleAtlas', 'arrow_origin');
+                arrowOrigin.anchor.setTo(0, 0.5);
+                arrowOrigin.angle = 90;
+                
+                arrowForward = arrowLayer.create(arrowOrigin.world.x, arrowOrigin.world.y+32, 'paddleAtlas', 'arrow_forward');
+                arrowForward.anchor.setTo(0, 0.5);
+                arrowForward.angle = 90;
+
+                arrowEnd = arrowLayer.create(arrowForward.world.x, arrowForward.world.y+64, 'paddleAtlas', 'arrow_end');
+                arrowEnd.anchor.setTo(0, 0.5);
+                arrowEnd.angle = 90;
+            }*/
+        }
+    }
+    /*if(yourTurn && !gameOver && !block){ //only works if it is the player's turn, the game isn't over, and there isn't a block in the way
         laddleTween = game.add.tween(laddle).to( { x: laddle.world.x, y: laddle.world.y + 64 }, 1000, "Linear", true); //move the player relative to its location slowly
         yourTurn = false; //end our turn
         score -= 50; //drop our score every move
         scoreText.text = 'Score: ' + score;
         laddleTween.onComplete.add(enemyTurn); //play the enemyTurn
-    }
+    }*/
 }
 
 function moveUp(){
