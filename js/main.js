5abd2bc14d13a21bf544b594e9c4dea147ce2692
Oscar Quinteros <oquinter@gmu.edu>
2016-05-03 21:56:09 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -317,8 +317,8 @@ function spawnEnemies(){ //creates a number of enemies and adds them into the wo
         var enemySpawn;
         currentSpace = spaces[(y+1) * rows - (rows - x)]; //get that space
         if (!(currentSpace.occupied)){ //if the space has nothing on it, create a new enemy
-            if (level > 8)
-                enemySpawn = 8;
+            if (level > 6)
+                enemySpawn = 6;
             else
                 enemySpawn = level;
             enemyType = Math.floor(Math.random() * enemySpawn);
